fcb8a62ba4eba80452ff6e9d1352f168b068ca9a
Muhammad Hasham <hasham25525@gmail.com>
2023-09-05 02:13:54 +0500

    metadata and font added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import Head from "next/head";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={plusJakartaSans.className}>
+      <Head>
+        <title>Epic Chat</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=1" />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
