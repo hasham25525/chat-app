1b57750ee0ea3a4be3fb68dd4abc82bc663aa71f
Muhammad Hasham <hasham25525@gmail.com>
2023-09-06 01:46:20 +0500

    auth user added to firestore db

---
+++ src/hooks/useAuthUsers.js
@@ -0,0 +1,27 @@
+"use client";
+import { auth, db } from "@/utils/firebase";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export default function useAuthUser() {
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      //"users" -> user.uid -> user(doc)
+      const userRef = doc(db, `users/${user.uid}`);
+      getDoc(userRef).then((snapshot) => {
+        if (!snapshot.exists()) {
+          setDoc(snapshot.ref, {
+            name: user.displayName,
+            photoURL: user.photoURL,
+            timestamp: serverTimestamp(),
+          });
+        }
+      });
+    }
+  }, [user]);
+
+  return user;
+}
