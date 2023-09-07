e11b8a8cc72bd41a081542babf3e69199637dcb5
Muhammad Hasham <hasham25525@gmail.com>
2023-09-08 00:49:14 +0500

    improved search

--- src/components/SidebarList.jsx
+++ src/components/SidebarList.jsx
@@ -1,21 +1,35 @@
 import { CircularProgress } from "@mui/material";
 import SidebarListItems from "./SidebarListItems";
+import { CancelOutlined, SearchOutlined } from "@mui/icons-material";
 
-const SidebarList = ({title,data}) => {
+const SidebarList = ({ title, data }) => {
+  if (!data) {
+    return (
+      <div className="loader__container sidebar__loader">
+        <CircularProgress />
+      </div>
+    );
+  }
 
-    if(!data){
-        return(
-            <div className="loader__container sidebar__loader">
-                <CircularProgress/>
-            </div>
-        )
-    }
+  if (!data.length && title === "Search Results") {
+    return (
+      <div className="no-result">
+        <div>
+          <SearchOutlined />
+          <div className="cancel-root">
+            <CancelOutlined />
+          </div>
+        </div>
+        <h2>no {title}</h2>
+      </div>
+    );
+  }
 
   return (
     <div className="sidebar__chat--container">
       <h2>{title}</h2>
-      {data.map(item=>(
-        <SidebarListItems key={item.id} item={item}/>
+      {data.map((item) => (
+        <SidebarListItems key={item.id} item={item} />
       ))}
     </div>
   );
